4cee4994ee0bdcf2ad02a5b23b53a7487438deab
UjHeavy <ujhelyi2002@gmail.com>
2021-11-19 12:54:46 +0100

    Órai munka, 2021. 11. 19.

--- ora10/my_list.py
+++ ora10/my_list.py
@@ -3,19 +3,20 @@ print("A lista hossza:", len(my_list))
 print("A len() függvény visszatérési értékének típusa:", type(len(my_list)))
 
 print("A 2 érték benne van-e a listában:", 2 in my_list)
+print("A 2.5 érték benne van-e a listában:", 2.5 in my_list)
 print("Az in operátor visszatérési értékének típusa:", type(2 in my_list))
 
 print("A 2.5 érték pozíciója a listában:", my_list.index(2.5))
 print("Az index metódus visszatérési értékének típusa:", type(my_list.index(2.5)))
+
 try:
     print("A 2 érték pozíciója a listában:", my_list.index(2))
 except ValueError:
-    print("Nincs ilyen elem a listában.")
+    print("A 2 érték nincs a listában.")
 
-print("A lista 1. eleme: ",my_list[0])
-print("A lista utolsó eleme: ", my_list[-1])
-print("A lista utolsó eleme: ",my_list[len(my_list)-1])
-
+print("A lista 1. eleme:", my_list[0])
+print("A lista utolsó eleme:", my_list[-1])
+print("A lista utolsó eleme:", my_list[len(my_list) - 1])
 print(my_list[0:2])
 print(type(my_list[0:2]))
 
@@ -33,3 +34,4 @@ print(my_list)
 
 my_list.insert(3, "szilva")
 print(my_list)
+
